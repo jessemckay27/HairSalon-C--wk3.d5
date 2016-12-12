194aba965aee1a1f6b6a116d9d2fa0bda3cd0470
Jesse <rosecity27@comcast.net>
2016-12-11 22:01:39 -0800

    add view for all stylists

--- HomeModules/Modules.cs
+++ HomeModules/Modules.cs
@@ -11,6 +11,10 @@ namespace HairSalonProject
       Get["/"] = _ =>{
         return View["index.cshtml"];
       };
+      Get["/stylists"] = _ =>{
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["stylists.cshtml", allStylists];
+      };
     }
   }
 }
