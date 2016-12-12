f3dc0d9b361d9d5f4cf4e5686c44d7b8a184e444
Jesse <rosecity27@comcast.net>
2016-12-12 01:50:43 -0800

    finish views, add basic styling

--- HomeModules/Modules.cs
+++ HomeModules/Modules.cs
@@ -34,18 +34,44 @@ namespace HairSalonProject
       Patch["/stylist/edit/{id}"] = parameters => {
         Stylist newStylist = Stylist.Find(parameters.id);
         newStylist.Update(Request.Form["name"]);
-      return View["success_edit.cshtml"];
+        return View["success_edit.cshtml"];
+      };
+      Get["/stylist/delete/{id}"] = parameters => {
+        Stylist newStylist = Stylist.Find(parameters.id);
+        return View["stylist_delete.cshtml", newStylist];
+      };
+      Delete["stylist/delete/{id}"] = parameters => {
+        Stylist newStylist = Stylist.Find(parameters.id);
+        newStylist.Delete();
+        return View["success_stylist_delete.cshtml"];
       };
       Get["/clients/new"]= _ =>{
-        List<Stylist> allClients = Stylist.GetAll();
-        return View["client_new.cshtml", allClients];
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["client_new.cshtml", allStylists];
       };
       Post["/clients/new"]= _ =>{
        Client newClient = new Client(Request.Form["name"], Request.Form["id"]);
        newClient.Save();
        return View["success_client.cshtml"];
       };
-
+      Get["/client_edit/{id}"] = parameters => {
+        Client newClient = Client.Find(parameters.id);
+        return View["client_edit.cshtml", newClient];
+      };
+      Patch["/client_edit/{id}"] = parameters => {
+        Client newClient = Client.Find(parameters.id);
+        newClient.Update(Request.Form["name"]);
+        return View["success_client.cshtml"];
+      };
+      Get["/client_delete/{id}"] = parameters => {
+       Client newClient = Client.Find(parameters.id);
+       return View["client_delete.cshtml", newClient];
+      };
+      Delete["/client_delete/{id}"] = parameters => {
+        Client newClient = Client.Find(parameters.id);
+        newClient.Delete();
+        return View["success_client_delete.cshtml"];
+      };
     }
   }
 }
