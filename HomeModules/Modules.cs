d9f6d97cccd1265c87f3939618ba02673a3f8355
Jesse <rosecity27@comcast.net>
2016-12-12 00:19:58 -0800

    add views for adding, editing for stylists and clients

--- HomeModules/Modules.cs
+++ HomeModules/Modules.cs
@@ -16,13 +16,36 @@ namespace HairSalonProject
         return View["stylists.cshtml", allStylists];
       };
       Get["/stylists/new"] = _ =>{
-        return View["new_stylist.cshtml"];
+        return View["stylist_new.cshtml"];
       };
       Post["/stylists/new"] = _ =>{
         Stylist newStylist = new Stylist(Request.Form["name"]);
         newStylist.Save();
         return View["success.cshtml"];
       };
+      Get["/stylist/{id}"] = parameters =>{
+       var newStylist = Stylist.Find(parameters.id);
+       return View["stylist_view.cshtml", newStylist];
+      };
+      Get["/stylist/edit/{id}"] = parameters =>{
+        Stylist newStylist = Stylist.Find(parameters.id);
+        return View["stylist_edit.cshtml", newStylist];
+      };
+      Patch["/stylist/edit/{id}"] = parameters => {
+        Stylist newStylist = Stylist.Find(parameters.id);
+        newStylist.Update(Request.Form["name"]);
+      return View["success_edit.cshtml"];
+      };
+      Get["/clients/new"]= _ =>{
+        List<Stylist> allClients = Stylist.GetAll();
+        return View["client_new.cshtml", allClients];
+      };
+      Post["/clients/new"]= _ =>{
+       Client newClient = new Client(Request.Form["name"], Request.Form["id"]);
+       newClient.Save();
+       return View["success_client.cshtml"];
+      };
+
     }
   }
 }
